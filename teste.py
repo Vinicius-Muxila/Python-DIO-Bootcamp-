609e1ec1bdf22575e77042a05eb2bb2c780996bb
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2024-08-27 22:38:06 -0300

    conversões

--- teste.py
+++ teste.py
@@ -1,2 +1,5 @@
 name, age, course, school = "Vinícius", 31, "análise e desenvolvimento de sistemas", "FIAP"
 print(f'Meu nome é {name}, tenho {age} anos e curso {course} na {school}.')
+
+print(5 // 2)
+float("a")
