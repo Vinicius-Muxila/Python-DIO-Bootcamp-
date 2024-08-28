e8918b140573a2883b9ab0ef9b20916ffa6eae45
Vinicius-Muxila <viniciuss.nuness@gmail.com>
2024-08-27 22:21:31 -0300

    testando print

--- teste.py
+++ teste.py
@@ -1,1 +1,2 @@
-print("Hello world")
+name, age, course, school = "Vinícius", 31, "análise e desenvolvimento de sistemas", "FIAP"
+print(f'Meu nome é {name}, tenho {age} anos e curso {course} na {school}.')
